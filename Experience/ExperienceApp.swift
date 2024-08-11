50f1a2b7077366fc07b5be25c37c1e33b5b70a68
Adrien <adrien.surugue@gmail.com>
2024-08-11 14:19:21 +0200

    Initial Commit

---
+++ Experience/ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceApp.swift
+//  Experience
+//
+//  Created by Adrien Surugue on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
